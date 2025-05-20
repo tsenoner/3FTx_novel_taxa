4217db451cbd8ff46b78a407e91e36994dd486ca
Selin Tuerkoglu <selin.t@live.de>
2025-05-20 12:25:06 +0200

    changes in checking af

--- src/data_processing/af2foldseek.py
+++ src/data_processing/af2foldseek.py
@@ -4,7 +4,7 @@ import os
 from tqdm import tqdm
 
 def check_alphafold(uniprot_id):
-    url = f"https://alphafold.ebi.ac.uk/files/AF-{uniprot_id}-F1-model_v1.pdb"
+    url = f"https://alphafold.ebi.ac.uk/files/AF-{uniprot_id}-F1-model_v4.pdb"
     r = requests.head(url)
     return r.status_code == 200
 
@@ -12,7 +12,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 fasta_file = os.path.join(script_dir, "../../data/interm/merged/merged_sanitized.fasta")
 fasta_file = os.path.normpath(fasta_file)
 
-records = list(SeqIO.parse(fasta_file, "fasta"))
+records = list(SeqIO.parse(fasta_file, "fasta"))[100:200] # 100 for debugging
 missing = []
 
 for record in tqdm(records, desc="Checking AlphaFold entries"):
@@ -20,5 +20,8 @@ for record in tqdm(records, desc="Checking AlphaFold entries"):
     if not check_alphafold(uniprot_id):
         missing.append(uniprot_id)
 
-print("\nNo AlphaFold structure for UniProt IDs:")
-print("\n".join(missing))
+if missing:
+    print("\nNo AlphaFold structure for UniProt IDs:")
+    print("\n".join(missing))
+else:
+    print("\nAll entries have AlphaFold structures.")
